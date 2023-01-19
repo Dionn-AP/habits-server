9f2e1c98812b23736675860732cfff4bd1e655cf
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-01-19 09:28:25 -0300

    Primeiro commit de todo o projeto até o momento

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { Routes } from './routes';
+
+const app = Fastify();
+
+app.register(cors);
+app.register(Routes);
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP Server running!')
+})
